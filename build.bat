74bbdc1033fb3c066bc53c3a20d8496c0faf89f5
geyson12345 <geysongomes6@gmail.com>
2024-10-02 10:43:29 -0300

    Criando instalacao da aplicacao AWS

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 861276095731.dkr.ecr.us-east-1.amazonaws.com
-docker build -t app-itau-rest -f Dockerfile .
-docker tag app-itau-rest:latest 861276095731.dkr.ecr.us-east-1.amazonaws.com/app-itau-rest:latest
-docker push 861276095731.dkr.ecr.us-east-1.amazonaws.com/app-itau-rest:latest
+docker build -t repository-itau -f Dockerfile .
+docker tag repository-itau:latest 861276095731.dkr.ecr.us-east-1.amazonaws.com/repository-itau:latest
+docker push 861276095731.dkr.ecr.us-east-1.amazonaws.com/repository-itau:latest
