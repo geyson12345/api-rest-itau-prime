55e0571d3a74c2e077b05556d4387ab182306af6
geyson12345 <geysongomes6@gmail.com>
2024-10-09 14:06:15 -0300

    Criando script no Terra-Form

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 861276095731.dkr.ecr.us-east-1.amazonaws.com
-docker build -t ecr-aws-itau -f Dockerfile .
-docker tag ecr-aws-itau:latest 861276095731.dkr.ecr.us-east-1.amazonaws.com/ecr-aws-itau:latest
-docker push 861276095731.dkr.ecr.us-east-1.amazonaws.com/ecr-aws-itau:latest
+docker build -t rpaws-itau -f Dockerfile .
+docker tag rpaws-itau:latest 861276095731.dkr.ecr.us-east-1.amazonaws.com/rpaws-itau:latest
+docker push 861276095731.dkr.ecr.us-east-1.amazonaws.com/rpaws-itau:latest
